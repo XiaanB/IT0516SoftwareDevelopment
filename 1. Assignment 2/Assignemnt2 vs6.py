ccdb44f66b29038ffefc7cb01d340305abb356e1
XiaanB <xiaan_b@hotmail.com>
2023-06-04 10:48:27 +1200

    Assignment 2 vs 4 respond to a ticket

--- 1. Assignment 2/Assignemnt2 vs6.py
+++ 1. Assignment 2/Assignemnt2 vs6.py
@@ -12,13 +12,19 @@ for number, selection in enumerate(available_selections):
     print("{0}: {1}".format(number, selection))
 print("_______________________________________________________")
 
+employee_id = ""
+employee_name = ""
+employee_email = ""
+employee_issue = ""
+ticket_status = ""
+ticket_response = ""
 
 ticket_number = 2000
 choice = "-"
-ticket_details_nested_dict = {ticket_number: {'ID': 'employee_id', 'Name': 'employee_name', 'Email': 'employee_email',
-                                              'Issue': 'employee_issue', 'Ticket status': 'ticket_status',
-                                              'Ticket response': 'ticket_response'}}
-
+ticket_details_nested_dict = {}
+# ticket_details_nested_dict = {ticket_number: {'ID': employee_id, 'Name': 'employee_name', 'Email': 'employee_email',
+#                                               'Issue': 'employee_issue', 'Ticket status': 'ticket_status',
+#                                               'Ticket response': 'ticket_response'}}
 
 while choice != "0":
     if choice == "1":
@@ -57,7 +63,8 @@ while choice != "0":
             print("__________________________________________________")
             print("Ticket Number:", key)
             for i in values:
-                print(key, " : ", i)
+                print(" : ", i)
+            print(ticket_details_nested_dict.items())
 
     if choice == "3":
         ticket_number = int(input("Please enter the ticket number you wish to edit: \n"))
@@ -65,8 +72,9 @@ while choice != "0":
         print(ticket_details_nested_dict[keys])
         employee_name = "John"
         ticket_details_nested_dict[keys]['Name'] = employee_name
-        #ticket_details_nested_dict[ticket_number]['ticket status'] = "resolved"
-        # ticket_details_nested_dict[ticket_number]['ticket_response'] = 'input("Enter you response")'
+        ticket_details_nested_dict[keys]['Ticket status'] = "resolved"
+        user_rsponse = input("Please enter your response: \n")
+        ticket_details_nested_dict[keys]['Ticket_response'] = user_rsponse
         print("All done")
         print("IT5014 Helpdesk Ticketing System:")
         print("_______________________________________________________")
