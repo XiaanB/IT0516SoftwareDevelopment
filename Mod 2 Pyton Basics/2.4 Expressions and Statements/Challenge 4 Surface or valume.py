4f1cdba793b6ae7e3dc2684056041ecf9b700362
XiaanB <95105148+XiaanB@users.noreply.github.com>
2023-06-30 07:07:36 +1200

    Update Challenge 4 Surface or valume.py
    Comments added

--- Mod 2 Pyton Basics/2.4 Expressions and Statements/Challenge 4 Surface or valume.py
+++ Mod 2 Pyton Basics/2.4 Expressions and Statements/Challenge 4 Surface or valume.py
@@ -1,11 +1,13 @@
+# Calculating the area of volume of a retangle. The user provide the mesuarements and they are stored in
+# variables. 
 hight = int(input("What is the hight of your cuboid? "))
 lenght = int(input("What is the lenght of your cuboid? "))
 width = int(input("What is the width of your cuboid? "))
 
+# Varaible to store the user selection
 selection = int(input("Please selct #1 if you would like to know the surface area or #2 if you would like to know the volume:"))
 
-
-
+# If statement to tell the program what to do from the user selection.
 if selection == 1:
     print("The surface area is:", (2*lenght*width) + (2*lenght*hight) + (2*hight*width))
 
