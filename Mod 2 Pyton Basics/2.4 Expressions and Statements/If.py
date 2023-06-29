654797cf35463b86feee13e1381bc84cf29df9f9
XiaanB <95105148+XiaanB@users.noreply.github.com>
2023-06-30 07:10:57 +1200

    Update If.py
    Adding comments

--- Mod 2 Pyton Basics/2.4 Expressions and Statements/If.py
+++ Mod 2 Pyton Basics/2.4 Expressions and Statements/If.py
@@ -1,10 +1,17 @@
+# Parking meter calculation
+# Print a greeting
 print("Kia Ora, this is a parking meter.")
 
+# Variable to store the user input and then pritning the user input back to him.
 park_time = int(input("How long did you park for?"))
 print("park_time time is ", park_time, "hours.")
+
+# Creating variables.
 rate = 4
 cost = 0
 
+# Doing the calculations. If parked for more than 3 hours on calculation and if 
+# parked for less than 3 hours there is another calculation.
 if park_time > 3:
     cost = rate*3
     rate -= 2
