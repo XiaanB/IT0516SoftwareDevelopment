7cddf515e77de50b04500b76833af8ebf7e4f741
XiaanB <95105148+XiaanB@users.noreply.github.com>
2023-07-01 10:13:15 +1200

    Update Q10 FizzBuzz.py
    Adding comments

--- Mod 2 Pyton Basics/2.5 Loops/Q10 FizzBuzz.py
+++ Mod 2 Pyton Basics/2.5 Loops/Q10 FizzBuzz.py
@@ -1,4 +1,5 @@
-
+# Creating a program to count up to a set number.
+# Evertime a setertain condition is met that number is replaced with a key word.
 
 for fizzbuzz in range(51):
     if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
