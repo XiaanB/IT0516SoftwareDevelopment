8c76dd5892fdcb79121980fa433d4751e7c0826b
XiaanB <95105148+XiaanB@users.noreply.github.com>
2023-07-01 10:58:33 +1200

    Update #3.py
    Adding comments and changing the program.

--- Mod 2 Pyton Basics/2.6 Funtions/#3.py
+++ Mod 2 Pyton Basics/2.6 Funtions/#3.py
@@ -1,27 +1,24 @@
 #Returning data
 
-number = int(input(print("Can you please enter a number>")))
-
-times = int(input(print("How many times")))
-
-
-print("Your number is " , number)
-
-
+# Creating a funtion with 2 arguments
 def multiples(number, times):
+    # Creating 2 variables, one for a counter and one for 
+    # start number
     counter = 1
-
     base = number
 
+    # A while loop using the funtion arguments to loop
+    # and add number. Also a counter counting up.
     while counter <= times:
-
         number += base
-
         counter += 1
-
         print(number)
 
+# Creating 2 variabes to store user input.
+user_number = int(input("Can you please enter a number: "))
+user_times = int(input("How many times: "))
 
-multiples(number, times)
-
+# Calling a funtion and using the 2 variables fom the user input as
+# arguments
+multiples(user_number, user_times)
 
