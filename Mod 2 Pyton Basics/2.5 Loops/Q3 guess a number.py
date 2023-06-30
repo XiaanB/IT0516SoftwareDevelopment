4e393df687a69509136126a81c193628443be30d
XiaanB <95105148+XiaanB@users.noreply.github.com>
2023-07-01 10:10:39 +1200

    Update Q3 guess a number.py
    Adding commnets

--- Mod 2 Pyton Basics/2.5 Loops/Q3 guess a number.py
+++ Mod 2 Pyton Basics/2.5 Loops/Q3 guess a number.py
@@ -1,6 +1,8 @@
 import random
 
+# Creating a variable to store a random number between 1 and 10
 number = random.randint(1, 10)
+# Creating a variable for the number of tries allowed.
 number_of_tries = 5
 
 while True:
@@ -8,14 +10,18 @@ while True:
     print("You have ", number_of_tries, " opportunities. ")
     user_input = int(input("Please enter a number between 0 and 10: "))
 
+    # Comaring the user input to the randome number.
     if user_input < 0 or user_input > 10:
         print("Please try a different number!")
+    # If the user input is not equal to the random number, reduce the number of tries.    
     if user_input != number:
         number_of_tries -= 1
         print("You have ", number_of_tries, " left. \n Tr again.")
+        # Defiuning what happens when the user tries are finished
         if number_of_tries == 0:
             print("You are out of tries, better luck next time...")
             break
+    # Printing a message and defining what happens if the user quess the right nuber.        
     if user_input == number:
         print("Well done")
         break
