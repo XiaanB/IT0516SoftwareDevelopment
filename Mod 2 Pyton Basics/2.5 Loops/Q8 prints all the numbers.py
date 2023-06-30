f7e67133b787c56b60590c2f5df9fc50a88e1ff2
XiaanB <95105148+XiaanB@users.noreply.github.com>
2023-07-01 10:00:09 +1200

    Update Q8 prints all the numbers.py
    Adding comments

--- Mod 2 Pyton Basics/2.5 Loops/Q8 prints all the numbers.py
+++ Mod 2 Pyton Basics/2.5 Loops/Q8 prints all the numbers.py
@@ -1,7 +1,9 @@
+# Creating a loop. Ginving it a start and en value.
 for i in range(0,7):
+    # If the given condition is met do a spisific action. 
     if i % 3 != 0:
         print(i)
-
+# Creating a loop and telling it the end.
 for x in range(6):
     if (x == 3 or x==6):
         continue
