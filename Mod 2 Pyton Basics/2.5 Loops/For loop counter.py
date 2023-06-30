18ded82fcf18e1bd06f055da677316c1ebacdd83
XiaanB <95105148+XiaanB@users.noreply.github.com>
2023-07-01 10:31:27 +1200

    Update For loop counter.py
    Adding comments

--- Mod 2 Pyton Basics/2.5 Loops/For loop counter.py
+++ Mod 2 Pyton Basics/2.5 Loops/For loop counter.py
@@ -1,15 +1,20 @@
 import time
 
+#For loop counting up from 1 to 10
 for i in range(10):
     print(i+1)
 
+# For loop counting from 50 to 100 in increments of 2.
 for i in range(50,100+1,2):
     print(i)
 
-for i in "christiaa":
+# For loop iterating over a string and printing each caracter
+for i in "christiaan":
     print(i)
 
+# For loop counting down from 10 to 1 in increments of 1
 for seconds in range(10,0,-1):
     print(seconds)
+    # Putting a dealy on the counter.
     time.sleep(1)
 print("happy new year")
