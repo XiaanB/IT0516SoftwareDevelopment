fcb62817c6e1c3c0914880ede830a1e6074018aa
XiaanB <95105148+XiaanB@users.noreply.github.com>
2023-07-01 10:15:48 +1200

    Update Q1 divisible by 7 and multiples of 5.py
    Adding comments

--- Mod 2 Pyton Basics/2.5 Loops/Q1 divisible by 7 and multiples of 5.py
+++ Mod 2 Pyton Basics/2.5 Loops/Q1 divisible by 7 and multiples of 5.py
@@ -1,8 +1,10 @@
+# Creating 2 variables to hold the numbers
 first_number = 1500
 second_number = 2700
 
-
+# Creating a for loop with the 2 variables as the start and end.
 for i in range(first_number, second_number):
+    # If both of the following conditions are met, do something (print)
     if (i % 7 == 0) and (i % 5 == 0):
         print(i, end=" ")
 
