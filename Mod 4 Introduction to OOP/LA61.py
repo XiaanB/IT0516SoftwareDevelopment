dd5875bbd053be9cdee0f00f08de0ddfe429da6e
XiaanB <95105148+XiaanB@users.noreply.github.com>
2023-07-01 13:16:45 +1200

    Update LA61.py

--- Mod 4 Introduction to OOP/LA61.py
+++ Mod 4 Introduction to OOP/LA61.py
@@ -1,10 +1,13 @@
+# Creating a class person with 4 properties
 class Person:
+    # INitializing 
     def __init__(self, age, gender, address, height):
         self.age = age
         self.gender = gender
         self.address = address
         self.height = height
 
+    # Fuction to display
     def display(self):
         print("age: ", self.age)
         print("gender: ", self.gender)
@@ -12,5 +15,7 @@ class Person:
         print("height: ", self.height)
 
 
+# Creating harry
 harry = Person(30, "Male", "califarnia", 1.79)
+# Useing the display function
 harry.display()
