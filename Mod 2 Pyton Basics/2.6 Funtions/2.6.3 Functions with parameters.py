72ad2ec066a45c83844afd97f9d73b8718821104
XiaanB <95105148+XiaanB@users.noreply.github.com>
2023-07-01 13:35:47 +1200

    Update 2.6.3 Functions with parameters.py

--- Mod 2 Pyton Basics/2.6 Funtions/2.6.3 Functions with parameters.py
+++ Mod 2 Pyton Basics/2.6 Funtions/2.6.3 Functions with parameters.py
@@ -1,14 +1,14 @@
-# def show_hello(param):
-#     print("Hello there, the number of times this"
-#           " function is called is {0}!!.".format(param))
-#
-#
-# counter = 0
-# print("testing my second user defined function.")
-#
-# counter += 1
-# show_hello(counter)
-#
-# counter += 1
-# show_hello(counter)
+def show_hello(param):
+    print("Hello there, the number of times this"
+          " function is called is {0}!!.".format(param))
+
+
+counter = 0
+print("testing my second user defined function.")
+
+counter += 1
+show_hello(counter)
+
+counter += 1
+show_hello(counter)
 
