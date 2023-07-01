842985f764637e42ffafb46350c76083fd98cc04
XiaanB <95105148+XiaanB@users.noreply.github.com>
2023-07-01 15:06:14 +1200

    Update strings.py

--- Mod 2 Pyton Basics/2.7 Strings/strings.py
+++ Mod 2 Pyton Basics/2.7 Strings/strings.py
@@ -1,1 +1,12 @@
+# Printing strings and using place holders
+string_1 = "Whanau!"
+ 
+print("The third letter is {0}\n".format(string_1[2]))
+print("The length of the string is {0}".format(len(string_1)))
 
+string2 = "Hello how are you?"
+
+print("The first letter is {0}".format(string2[0]))
+print("the lenght is {0}".format(len(string2)))
+print("The 8 th letter is  {0}, exluding spaces".format(string2[8]))
+print("The second, {0}  and 8th letters, {1} are ".format(string2[1], string2[7] ))
