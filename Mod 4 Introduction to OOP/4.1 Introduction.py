a56e464d61c3e8cfb2a3f4e9103277ab1a403f7b
XiaanB <95105148+XiaanB@users.noreply.github.com>
2023-07-01 13:14:59 +1200

    Update 4.1 Introduction.py

--- Mod 4 Introduction to OOP/4.1 Introduction.py
+++ Mod 4 Introduction to OOP/4.1 Introduction.py
@@ -1,13 +1,17 @@
+# Creating class
 class Person:
+    # Use the __init__() function to assign values to object properties, or other operations 
+    # that are necessary to do when the object is being created
     def __init__(self, age, gender):
         self.age = age
         self.gender = gender
 
+    # Creating a function
     def my_intro(self):
         print("Hello my gender is " + self.gender)
         print('Hello my age is ', self.age)
 
-
+# Using the class to create instances
 harry = Person(36, "Male")
 Sarrah = Person(24, "Female")
 
