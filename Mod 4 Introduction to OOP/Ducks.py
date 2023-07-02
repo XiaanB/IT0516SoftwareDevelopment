dfa32e05d5689e53aa7060e86f4ebc37d33f2a7e
XiaanB <95105148+XiaanB@users.noreply.github.com>
2023-07-02 14:57:48 +1200

    Create Ducks.py

---
+++ Mod 4 Introduction to OOP/Ducks.py
@@ -0,0 +1,35 @@
+class Duck(object):
+
+    def walk(self):
+        print("Waddle, waddle, waddle")
+
+    def swim(self):
+        print("Come on it, the water's lovely")
+
+    def quack(self):
+        print("Quack quack")
+
+
+class Penguin(object):
+
+    def walk(self):
+        print("Waddle, waddle, I waddle too")
+
+    def swim(self):
+        print("Come on in, but it's a bit chilly this far South")
+
+    def quack(self):
+        print("Are you 'avin' a larf? I'm a penguin!")
+
+
+def test_duck(duck):
+    duck.walk()
+    duck.swim()
+    duck.quack()
+
+if __name__ == '__main__':
+    donald = Duck()
+    test_duck(donald)
+
+    percy = Penguin()
+    test_duck(percy)
